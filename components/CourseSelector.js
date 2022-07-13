d26ed982b771ef50753fb72619252c2b0a6f5bcc
emilykohlberg <emilykohlberg@Emilys-MacBook-Pro-7.local>
2022-07-13 13:31:54 -0500

    add a course detail screen

--- components/CourseSelector.js
+++ components/CourseSelector.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { hasConflict } from '../utils/course.js'
 
-const CourseSelector = ({courses}) => {
+const CourseSelector = ({courses, view}) => {
     const [selected, setSelected] = useState([]);
   
     const toggle = course => setSelected(selected => (
@@ -18,6 +18,7 @@ const CourseSelector = ({courses}) => {
               isDisabled={hasConflict(course, selected)}
               isSelected={selected.includes(course)}
               select={toggle}
+              view={view}
             />
           ))
         }
